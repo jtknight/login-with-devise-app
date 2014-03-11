5698192760f182cbeb05f1d22bb6579d1d06671b
Josh Knight <joshua.t.knight@gmail.com>
2014-03-10 20:10:08 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Ruby on Rails Practice: Authentication with Devise
+
+This is a sample application to
+test using the Devise gem for Rails.
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DeviseLoginApp::Application.config.session_store :cookie_store, key: '_devise_login_app_session'
