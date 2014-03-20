18adb5b43bef5569597a3bfb0d8a33351c371a0b
Josh Knight <joshua.t.knight@gmail.com>
2014-03-20 16:54:13 -0400

    Add static pages for home, contact us, and signup, as well as a Users controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 DeviseLoginApp::Application.routes.draw do
+  
+  get "users/new"
+  root 'static_pages#home'
+  match '/signup',  to: 'users#new',            via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
